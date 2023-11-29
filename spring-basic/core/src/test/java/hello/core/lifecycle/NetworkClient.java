f7f53d17be9db05b00c74ce0b91128c5048a8d13
unknown <qrdlsgh1234@khu.ac.kr>
2023-11-29 17:39:28 +0900

    EDIT: section9

--- spring-basic/core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ spring-basic/core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,10 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
 public class NetworkClient {
 
     private String url;
@@ -25,5 +30,19 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close " + url);
+    }
+
+
+    @PostConstruct
+    public void init() throws Exception {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        System.out.println("NetworkClient.close");
+        disconnect();
     }
 }
