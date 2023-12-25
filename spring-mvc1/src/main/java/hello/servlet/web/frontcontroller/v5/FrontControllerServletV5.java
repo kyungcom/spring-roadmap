19c35e6a7065fbf8f695132e79a0e250c91205f4
rudgmleo17@ <qrdlsgh1234@khu.ac.kr>
2023-12-25 21:34:14 +0900

    EDIT: finish section4

--- spring-mvc1/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
+++ spring-mvc1/src/main/java/hello/servlet/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -11,6 +11,7 @@ import hello.servlet.web.frontcontroller.v4.controller.MemberFormControllerV4;
 import hello.servlet.web.frontcontroller.v4.controller.MemberListControllerV4;
 import hello.servlet.web.frontcontroller.v4.controller.MemberSaveControllerV4;
 import hello.servlet.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+import hello.servlet.web.frontcontroller.v5.adapter.ControllerV4HandlerAdapter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -25,50 +26,52 @@ import java.util.Map;
 
 @WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
 public class FrontControllerServletV5 extends HttpServlet {
-
     private final Map<String, Object> handlerMappingMap = new HashMap<>();
     private final List<MyHandlerAdapter> handlerAdapters = new ArrayList<>();
-
     public FrontControllerServletV5() {
         initHandlerMappingMap();
         initHandlerAdapters();
     }
-
     private void initHandlerMappingMap() {
-        handlerMappingMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
-        handlerMappingMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
-        handlerMappingMap.put("/front-controller/v3/members", new MemberListControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v4/members/new-form", new MemberFormControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members/save", new MemberSaveControllerV4());
+        handlerMappingMap.put("/front-controller/v5/v4/members", new MemberListControllerV4());
     }
-
     private void initHandlerAdapters() {
         handlerAdapters.add(new ControllerV3HandlerAdapter());
+        handlerAdapters.add(new ControllerV4HandlerAdapter());
     }
-
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
         Object handler = getHandler(request);
 
-        if (handler == null) {
-            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            return;
-        }
-        //paramMap
+         if (handler == null) {
+             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+             return;
+         }
 
-        for (MyHandlerAdapter adapter : handlerAdapters) {
-            if (adapter.supports(handler))
-        }
-
-        Map<String, String> paramMap = createParamMap(request);
-        ModelView mv = controller.process(paramMap);
-
-        String viewName = mv.getViewName();
-        MyView view = viewResolver(viewName);
-
-        view.render(mv.getModel(), request, response);
+         MyHandlerAdapter adapter = getHandlerAdapter(handler);
+         ModelView mv = adapter.handle(request, response, handler);
+         MyView view = viewResolver(mv.getViewName());
+         view.render(mv.getModel(), request, response);
+     }
+     private Object getHandler(HttpServletRequest request) {
+         String requestURI = request.getRequestURI();
+         return handlerMappingMap.get(requestURI);
+}
+     private MyHandlerAdapter getHandlerAdapter(Object handler) {
+         for (MyHandlerAdapter adapter : handlerAdapters) {
+             if (adapter.supports(handler)) {
+                 return adapter;
+             }
+         }
+        throw new IllegalArgumentException("handler adapter를 찾을 수 없습니다. handler=" + handler);
     }
-
-    private Object getHandler(HttpServletRequest request) {
-        String requestURI = request.getRequestURI();
-        return handlerMappingMap.get(requestURI);
+     private MyView viewResolver(String viewName) {
+         return new MyView("/WEB-INF/views/" + viewName + ".jsp");
     }
 }
