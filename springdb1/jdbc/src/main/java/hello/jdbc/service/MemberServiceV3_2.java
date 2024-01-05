17ca309213a8b165013e63f672591b08b5b65d47
unknown <qrdlsgh1234@khu.ac.kr>
2024-01-05 10:32:21 +0900

    EDIT: finish spring transaction basic

--- springdb1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
+++ springdb1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_2.java
@@ -2,31 +2,30 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.sql.SQLException;
 
 @Slf4j
-@RequiredArgsConstructor
-public class MemberServiceV3_1 {
+public class MemberServiceV3_2 {
 
-    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate txTemplate;
     private final MemberRepositoryV3 memberRepository;
-
+    public MemberServiceV3_2(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepository) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepository = memberRepository;
+    }
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        try {
-            //비즈니스 로직
-            bizLogic(fromId, toId, money);
-            transactionManager.commit(status); //성공시 커밋
-        } catch (Exception e) {
-            transactionManager.rollback(status); //실패시 롤백
-            throw new IllegalStateException(e);
-        }
+        txTemplate.executeWithoutResult((status) -> {
+            try {
+                //비즈니스 로직
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
     }
     private void bizLogic(String fromId, String toId, int money) throws
             SQLException {
@@ -41,5 +40,4 @@ public class MemberServiceV3_1 {
             throw new IllegalStateException("이체중 예외 발생");
         }
     }
-
 }
--- springdb1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
+++ springdb1/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_3.java
@@ -4,30 +4,20 @@ import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepositoryV3;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 
 @Slf4j
 @RequiredArgsConstructor
-public class MemberServiceV3_1 {
-
-    private final PlatformTransactionManager transactionManager;
+public class MemberServiceV3_3 {
     private final MemberRepositoryV3 memberRepository;
 
+    @Transactional
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        try {
-            //비즈니스 로직
-            bizLogic(fromId, toId, money);
-            transactionManager.commit(status); //성공시 커밋
-        } catch (Exception e) {
-            transactionManager.rollback(status); //실패시 롤백
-            throw new IllegalStateException(e);
-        }
+        bizLogic(fromId, toId, money);
     }
+
     private void bizLogic(String fromId, String toId, int money) throws
             SQLException {
         Member fromMember = memberRepository.findById(fromId);
@@ -36,10 +26,10 @@ public class MemberServiceV3_1 {
         validation(toMember);
         memberRepository.update(toId, toMember.getMoney() + money);
     }
+
     private void validation(Member toMember) {
         if (toMember.getMemberId().equals("ex")) {
             throw new IllegalStateException("이체중 예외 발생");
         }
     }
-
 }
