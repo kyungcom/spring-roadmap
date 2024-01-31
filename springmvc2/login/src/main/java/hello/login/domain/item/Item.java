19fd7f4e0e7ffa7c4009c1d4742683530ac769c3
unknown <qrdlsgh1234@khu.ac.kr>
2024-01-31 17:01:44 +0900

    EDIT: do filter

--- springmvc2/login/src/main/java/hello/login/domain/item/Item.java
+++ springmvc2/login/src/main/java/hello/login/domain/item/Item.java
@@ -1,12 +1,6 @@
-package hello.itemservice.domain.item;
+package hello.login.domain.item;
 
 import lombok.Data;
-import org.hibernate.validator.constraints.Range;
-import org.hibernate.validator.constraints.ScriptAssert;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class Item {
