98826131e24aa191baf8129a70de4017e53b3080
unknown <qrdlsgh1234@khu.ac.kr>
2024-02-06 17:04:42 +0900

    EDIT: converter

---
+++ springmvc2/typeconverter/src/test/java/hello/typeconverter/converter/ConversionServiceTest.java
@@ -0,0 +1,22 @@
+package hello.typeconverter.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.core.convert.support.DefaultConversionService;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConversionServiceTest {
+
+    @Test
+    void conversionService() {
+        DefaultConversionService conversionService = new DefaultConversionService();
+
+        conversionService.addConverter(new StringToIntegerConverter());
+        conversionService.addConverter(new StringToIpPortConverter());
+        conversionService.addConverter(new IntegerToStringConverter());
+        conversionService.addConverter(new IpPortToStringConverter());
+
+        assertThat(conversionService.convert("10", Integer.class)).isEqualTo(10);
+        assertThat(conversionService.convert(10, String.class)).isEqualTo("10");
+    }
+}
